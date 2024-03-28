c1f19e642ead7d2764c776ce369ecdcdb0307ea4
emongo <emongochadrack@gmail.com>
2024-03-28 16:52:18 +0200

    Recupration des Admin dans la base de données et les afficher du coté client

--- client/src/ListeAdmin.jsx
+++ client/src/ListeAdmin.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from './Title'
 import Button from './Button'
 import Option from './Option'
+import axios from 'axios'
 
 export default function ListeAdmin() {
+  const [admin, setAdmin] = useState([])
+  let [numero,setNumero]= useState(1)
+  useEffect(() => {
+    axios.get('http://localhost:8002/readAdmin')
+      .then(response => {
+        setAdmin(response.data);
+      })
+  }, ['admin','numero']);
+  
   return (
     <div className='p-5 -ml-5'>
       <div>
@@ -11,6 +21,28 @@ export default function ListeAdmin() {
           <div className='font-[lemon] text-2xl text-slate-950 mb-5 '>
             <Title Title="La liste des Admins" />
           </div>
+          <table>
+            <thead>
+              <tr>
+                <th>N°</th>
+                <th>Nom</th>
+                <th>Post-Nom</th>
+                <th>Téléphone</th>
+                <th>E-mail</th>
+              </tr>
+            </thead>
+            <tbody>
+              {admin.map((admin) => (
+                <tr key={admin.id_admin}>
+                  <td>{numero++}</td>
+                  <td>{admin.nom}</td>
+                  <td>{admin.postnom}</td>
+                  <td>{admin.telephone}</td>
+                  <td>{admin.email}</td>
+                </tr>))
+              }
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
--- client/src/ListeClient.jsx
+++ client/src/ListeClient.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from './Title'
 import Button from './Button'
 import Option from './Option'
-import Form from './Form'
-import Inputlabel from './Inputlabel'
+import axios from 'axios'
 
-
-export default function ListeClient() {
+export default function ListeAdmin() {
+  const [admin, setAdmin] = useState([])
+  let [numero,setNumero]= useState(1)
+  useEffect(() => {
+    axios.get('http://localhost:8002/readAdmin')
+      .then(response => {
+        setAdmin(response.data);
+      })
+  }, ['admin','numero']);
+  
   return (
     <div className='p-5 -ml-5'>
       <div>
         <div className='bg-white -mt-4 -mb-10 p-6 flex justify-center'>
-          <div className='font-[lemon] text-2xl text-slate-950 mb-5 '>
-            <Title Title="La liste des clients" />
+          <div className='font-[lemon] text-xl text-slate-950 mb-5 '>
+            <Title Title="La liste des Admins" />
           </div>
+          <table>
+            <thead>
+              <tr className='w-full'>
+                <th className='text-blue-300 w-10 border border-solid border-2 border-indigo-600 '>N°</th>
+                <th className='text-red-200'>Nom</th>
+                <th>Post-Nom</th>
+                <th>Téléphone</th>
+                <th>E-mail</th>
+              </tr>
+            </thead>
+            <tbody>
+              {admin.map((admin) => (
+                <tr key={admin.id_admin}>
+                  <td>{numero++}</td>
+                  <td>{admin.nom}</td>
+                  <td>{admin.postnom}</td>
+                  <td>{admin.telephone}</td>
+                  <td>{admin.email}</td>
+                </tr>))
+              }
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
